a8bcac76355b80014a362ff2ac479b5fbc60101a
Jordan Shaw <jordan.shaw@textnow.com>
2024-06-02 19:55:41 -0500

    fix build script
    Signed-off-by: Jordan Shaw <jordan.shaw@textnow.com>

--- pzzl-database/build.sh
+++ pzzl-database/build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -x
+
 AWS_PROFILE=default
 AWS_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
-VERSION=${3:-latest}
+VERSION=${1:-latest}
+
+cargo clean
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com
 
-docker build -t pzzl/database . --progress=plain
+docker build -t pzzl/database:$VERSION . --progress=plain
 docker tag pzzl/database:$VERSION $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/pzzl/database:$VERSION
 docker push $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/pzzl/database:$VERSION
 
